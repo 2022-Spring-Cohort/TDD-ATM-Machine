3f43723450ee2a54e8805bc7413a82ec158a40e8
Carlos Lopez <carlos@wecancodeit.org>
2022-01-27 15:28:58 -0500

    Added Computer Example

---
+++ Computer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMMachine
+{
+    class Computer
+    {
+        List<Part> Parts { get; set; } = new List<Part>();
+    }
+
+    class Part
+    {
+        string Name { get; set; }
+        double Cost { get; set; }
+    }
+
+    // Example Parts
+    // new Part(){ Name = "Graphics Card", Cost = 500.0 };
+}
--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@ namespace ATMMachine
 
             myBank.Menu();
 
-            
 
         }
     }
